088e86c966a039996d5c297d9f89f2a70823ee9f
Craig McClanahan <craigmcc@gmail.com>
2021-10-27 19:42:33 -0700

    OAuth support and basic Express application.

---
+++ src/routers/ApiRouter.ts
@@ -0,0 +1,31 @@
+// ApiRouter -----------------------------------------------------------------
+
+// Consolidation of Routers for REST APIs for application Models.
+
+// External Modules ----------------------------------------------------------
+
+import {Router} from "express";
+
+// Internal Modules ----------------------------------------------------------
+
+//import CategoryRouter from "./CategoryRouter";
+//import ClientRouter from "./ClientRouter";
+//import DetailRouter from "./DetailRouter";
+//import FacilityRouter from "./FacilityRouter";
+//import UserRouter from "./UserRouter";
+
+// Public Objects ------------------------------------------------------------
+
+export const ApiRouter = Router({
+    strict: true,
+});
+
+export default ApiRouter;
+
+// Model Specific Routers ----------------------------------------------------
+
+//ApiRouter.use("/categories", CategoryRouter);
+//ApiRouter.use("/client", ClientRouter);
+//ApiRouter.use("/details", DetailRouter);
+//ApiRouter.use("/facilities", FacilityRouter);
+//ApiRouter.use("/users", UserRouter);
