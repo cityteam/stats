b63ff37580ca1ebbafb128f67725f6355bfd3f6c
Craig McClanahan <craigmcc@gmail.com>
2022-03-10 12:37:44 -0800

    Bump some client side versions.

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders CityTeam Stats branding', () => {
   render(<App />);
   const linkElement = screen.getByText("CityTeam Stats");
   expect(linkElement).toBeInTheDocument();
