720c30b40b87ec729926a79fc8b1c3b81d73846e
Craig McClanahan <craigmcc@gmail.com>
2022-04-23 10:33:01 -0700

    Do not load Apache ECharts things in test mode.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@
 
 // External Modules ----------------------------------------------------------
 
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import {isMobile, useMobileOrientation} from "react-device-detect";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import {ToastContainer} from "react-toastify";
@@ -20,9 +20,7 @@ import FacilitySegment from "./components/facilities/FacilitySegment";
 import HomeView from "./components/general/HomeView";
 import Navigation from "./components/general/Navigation";
 import ConfigurationReport from "./components/reports/ConfigurationReport";
-import MonthlyChart from "./components/reports/MonthlyChart";
 import MonthlyReport from "./components/reports/MonthlyReport";
-import YearlyChart from "./components/reports/YearlyChart";
 import YearlyReport from "./components/reports/YearlyReport";
 import SectionSegment from "./components/sections/SectionSegment";
 import UserSegment from "./components/users/UserSegment";
@@ -30,7 +28,13 @@ import {FacilityContextProvider} from "./components/facilities/FacilityContext";
 import {LoginContextProvider} from "./components/login/LoginContext";
 import logger from "./util/ClientLogger";
 
-function App() {
+// Apache ECharts is a pure ESM module, which Jest has problems with,
+// so do not import the modules using it when in test mode.
+const MonthlyChart = lazy(() => import("./components/reports/MonthlyChart"));
+const YearlyChart = lazy(() => import("./components/reports/YearlyChart"));
+const notTestMode = "test" !== process.env.NODE_ENV;
+
+function App () {
 
     const {isLandscape} = useMobileOrientation();
     logger.info({
@@ -61,8 +65,20 @@ function App() {
                               <Route path="admin-sections" element={<SectionSegment/>}/>
                               <Route path="admin-users" element={<UserSegment/>}/>
                               <Route path="entries" element={<EntriesView/>}/>
-                              <Route path="chart-month" element={<MonthlyChart/>}/>
-                              <Route path="chart-year" element={<YearlyChart/>}/>
+                              {(notTestMode) ? (
+                                  <Route path="chart-month" element={
+                                      <Suspense fallback={<div>Loading MonthlyChart</div>}>
+                                        <MonthlyChart/>
+                                      </Suspense>
+                                  }/>
+                              ) : null }
+                              {(notTestMode) ? (
+                                  <Route path="chart-year" element={
+                                      <Suspense fallback={<div>Loading YearlyChart</div>}>
+                                          <YearlyChart/>
+                                      </Suspense>
+                                  }/>
+                              ) : null }
                               <Route path="report-configuration" element={<ConfigurationReport/>}/>
                               <Route path="report-month" element={<MonthlyReport/>}/>
                               <Route path="report-year" element={<YearlyReport/>}/>
